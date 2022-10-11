3625b617331788d6c4f38f8269dc2a411b29e75c
Abyravka90 <asepcahya.nugraha75@gmail.com>
2022-10-11 08:30:28 +0700

    latest

---
+++ DataTypes_1.php
@@ -0,0 +1,15 @@
+<?php
+// arithmetic
+$a = '10';
+$b = 10;
+$c = $a % $b;
+print 'hasil a+b='.$c."\n";
+echo "=====================\n";
+
+//luas lingkaran
+define("phi", 3.14);
+$r = 10;
+$luasLingkaran = phi*pow($r,2);
+print "luas lingkaran dengan \n r=10 \n luas=".$luasLingkaran; 
+echo "\n=====================\n";
+
